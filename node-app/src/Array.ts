08b0a8c38218876ab118b723ee717ec38c518a6c
kunnal-tyagi <kunaltyyagi@gmail.com>
2025-10-25 13:22:42 +0000

    ts

--- node-app/src/Array.ts
+++ node-app/src/Array.ts
@@ -1,10 +1,16 @@
 //given  an array of +ve integers return maxvalue
 
-function maxvalue(val:number[]){
-    let maxi=0;
-    for(let i=0;i<val.length;i++){
-        if(val[i]>maxi) maxi=val[i];
+function maxvalue(val: number[]): number | undefined {
+    if (val.length === 0) return undefined;
+// ! = non-null assertion operator → you tell TypeScript: 
+// “I promise this value is not undefined.”   
+//  let maxi = val[0]! ; // guaranteed number
+    for (let i = 1; i < val.length; i++) {
+        if (val[i]!== undefined && val[i]! > maxi) maxi = val[i]!;
     }
     return maxi;
 }
-console.log(maxvalue([19,2,212,7]));
+
+
+console.log(maxvalue([19, 2, 212, 7])); // 212
+console.log(maxvalue([]));              // undefined
